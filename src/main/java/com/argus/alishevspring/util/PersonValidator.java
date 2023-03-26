b41058d7c6710dc88e934e123eb198e4d18f00c7
ArgusWarp <arguswarp@gmail.com>
2023-03-26 21:08:57 +0300

    Initial commit, template project to start

---
+++ src/main/java/com/argus/alishevspring/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package com.argus.alishevspring.util;
+
+import com.argus.alishevspring.dao.PersonDAO;
+import com.argus.alishevspring.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (personDAO.show(person.getEmail()).isPresent()) {
+            errors.rejectValue("email", "", "This email has already been taken");
+        }
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>This is start page</h2>
+<a href="/people">To people</a>
+</body>
+</html>
